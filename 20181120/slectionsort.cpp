8322e3f0695105455e70e0303a3c567412982028
cp2015440038 <kygrla@naver.com>
2018-11-20 14:02:43 +0900

    수정

--- 20181120/slectionsort.cpp
+++ 20181120/slectionsort.cpp
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <windows.h>
 
-void printArray(int* arr, int len){ // 앞으로 배열을 함수에 전달할 경우 이 문법을 주로 사용한다.
-	for(int index = 0; index<len; index++){
+void printArray(int* arr, int len) { // 앞으로 배열을 함수에 전달할 경우 이 문법을 주로 사용한다.
+	for (int index = 0; index < len; index++) {
 		printf("array[%d] = %d \n ", index, arr[index]);
 	}
 }
 /* ... */
-void printArray(int* arr, int len){ ..... }
+//void printArray(int* arr, int len) { ..... }
 // 함수 : findMinIndex()
 //입력 : 배열(배열이름,배열길이)
 //출력 : 최소값의 인덱스
-int findMinIndex(int* arr, int length){
+int findMinIndex(int* arr, int length) {
 	int minIndex;
+	int min = arr[0];
+	for (int count = 0; count < length; count++) {
+		if (min > arr[count])
+			minIndex = count;
+	}
+	
 
 	return minIndex;
 
@@ -22,9 +28,9 @@ int findMinIndex(int* arr, int length){
 // 함수 : findMin()
 //입력 : 배열(배열이름,배열길이)
 //출력 : 배열 최소값
-int findMin(int* arr, int length){
-	int min=arr[0];
-	for ( int count = 1; count<length; count++){
+int findMin(int* arr, int length) {
+	int min = arr[0];
+	for (int count = 1; count < length; count++) {
 		if (min > arr[count])
 			min = arr[count];
 	}
@@ -55,18 +61,18 @@ void selectionSort(int * arr, int length) {
 		for (j = i; j < length; j++)
 		{
 			findMin(arr, length);
-			Index = findMinindex(arr, length);
+			Index = findMinIndex(arr, length);
 		}
 		swapElement(arr, i, Index);
 	}
 }
 
 int main() {
-	int a[] = {30,35,27,15,40};
+	int a[] = { 30,35,27,15,40 };
 
-	printf("minimum value of the array is %d\n", findMin(a, 5));
-	printf("minimum index = %d\n", findMinindex(a, 5));
+	printf("최솟값 %d\n", findMin(a, 5));
+	printf("최소 인덱스 = %d\n", findMinIndex(a, 5));
 	selectionSort(a, 5);
 	printArray(a, 5);
 }
-system("pause");
+
